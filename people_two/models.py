3b9355b56d2ec3489747a090625b8a82c0bf6aa2
Андрей <gopertio33@gmail.com>
2024-11-11 16:22:45 +0400

    хз

--- people_two/models.py
+++ people_two/models.py
@@ -1,3 +1,29 @@
+from datetime import datetime
+from types import new_class
+
 from django.db import models
 
-# Create your models here.
+class Status():
+    new = 'new'
+    in_progress = 'in_progress'
+    done = 'done'
+
+class Task():
+    def __init__(self, id, project, description, status, assigne, created_at, updated_at):
+        self.id = id
+        self.project = project
+        self.description = description
+        self.status = status
+        self.assigne = assigne
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+
+    def update_status(self, status):
+        if isinstance(status, Status):
+            self.status = status
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+        return f'task:{self.id}(title:{self.title}, status:{self.status}, assigne:{self.assigne})'
+
