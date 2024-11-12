1a33c435ab4f9831857c9ab6caf3d2bb6d49c3d3
LegendaBlyat <toni.druzhinin.2018@gmail.com>
2024-11-12 12:10:14 +0400

    Добавил шаблоны восстановления пароля

--- Management/urls.py
+++ Management/urls.py
@@ -1,23 +1,20 @@
-
-from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.contrib import admin
 from django.urls import path, include
 from app.views import registration
 from django_email_verification import urls as email_urls
-
-
-
-
-
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('admin/', admin.site.urls),
-    path('admin/', admin.site.urls),
     path('user/registration/', registration),
-    path('admin/', admin.site.urls),
-    path('user/registration/', registration),
-    path('email/', include(email_urls)),
+    path('reset_password/', auth_views.PasswordResetView.as_view(),
+         name='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(),
+         name='password_reset_complete'),
+
 ]
